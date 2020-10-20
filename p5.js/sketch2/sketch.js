20c11d2fd0be90deb2cd9d8e0b27f05c3063dd22
Megan Tucker <71663638+mntucker@users.noreply.github.com>
2020-10-20 12:20:58 -0700

    Adding sketches

---
+++ p5.js/sketch2/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(480, 220);
+    }
+    function draw() {
+    background(128, 0, 128);
+    strokeWeight(8); // Stroke weight to 8 pixels
+    fill(0, 128, 128); // Teal color
+    ellipse(240, -50, 400, 400);
+    fill(128, 128, 128); // Dark Gray color
+    ellipse(100, 163, 110, 110);
+    fill(192, 192, 192); // Green color
+    ellipse(240, 185, 70, 70);
+    fill(128, 128, 128); // Dark Gray color
+    ellipse(380, 163, 110, 110);
+    }
