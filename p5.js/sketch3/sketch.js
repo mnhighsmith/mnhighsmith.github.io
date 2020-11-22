7ecd95210f650b0a5e1a88fec3e02d7e5176991b
Megan Tucker <71663638+mntucker@users.noreply.github.com>
2020-11-22 11:47:43 -0800

    Sketch 3

--- p5.js/sketch3/sketch.js
+++ p5.js/sketch3/sketch.js
@@ -1,30 +1,32 @@
-let flower;
 
-//preload
-function preload() {
-  flower = loadImage('flower.pgn.gif');
-
-}
-//close preload
 
 function setup() {
-  createCanvas(480, 480);
-
-}
-
-
-// Draw
-function draw() {
-  fill(51, 63, 220);
-  rect(0, 0, 480, 400);
-  fill(0, 255, 0);
-  rect(0, 400, 480, 80);
-  fill(65, 133, 28);
-  fill(255, 250, 64);
-  ellipse(40, 40, 80);
-  noStroke();
-  image(flower, 0, 0);
-  if (mouseIsPressed == true) {
-    ellipse(mouseX, mouseY, 9, 9);
+    createCanvas(480, 480);
+    textSize (20);
+  
+    fill(129, 215, 242);
+    rect(0, 0, 480, 400);
+    fill(0, 255, 0);
+    rect(0, 400, 480, 80);
+    fill(255, 250, 64);
+    ellipse(40, 40, 80);
+    fill(230,236,55)
+    ellipse (240,320,40)
+    noStroke();
+    strokeWeight(3);
+    stroke (117, 180, 0);
+    line(240,340,240, 400);
+  
   }
-}
+  let s = 'Draw the petals on the flower.';
+  
+  function draw(){
+  fill (50);
+  text(s, 200, 10, 100, 80);}
+  function mouseDragged() 
+  { 
+  strokeWeight(6);
+  fill(117, 180, 0)
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+  
